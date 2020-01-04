7fda854a2b154207c1a859811a38a9ddb19619e1
JackGriffiths <jackdotgriffiths@gmail.com>
2020-01-04 12:26:40 +0000

    Environment Generation on Server started

--- game-server.js
+++ game-server.js
@@ -1,29 +1,29 @@
 var http = require('http');
-
+var express = require('express');
 var finalhandler = require('finalhandler');
 var serveStatic = require('serve-static');
 
 var serve = serveStatic("./");
+var app = express();
+var currentLevel = "1";
+var nextLevel = "2";
 
-var server = http.createServer(function(req, res) {
-  var done = finalhandler(req, res);
-  serve(req, res, done);
+
+app.use(express.static('./'));//Serving static file
+
+app.listen(9000, function() { //Listener for specified port
+    //setInterval(createLevel,1500);
+    console.log("Server running at: http://localhost:" + 9000)
 });
 
-server.listen(9000, function() {
-  console.log("Listening on " + 9000);
-  createLevel();
+
+app.get('/data', function(req, res) {
+    res.send(nextLevel)
 });
-
-var levelLength = 100;
-var level = "1";
-
-
 
 function createLevel(){
-  for (var i = 0; i < levelLength; i++) {
-    var randomInt = Math.floor(Math.random() * Math.floor(5)) + 1;
-    level += randomInt.toString();
-  }
-  console.log(level);
+  currentLevel = nextLevel;
+  var randomInt = Math.floor(Math.random() * Math.floor(5)) + 1;
+  nextLevel = randomInt;
+  console.log("Current Level is " + currentLevel + ". Next Level is " + nextLevel);
 }

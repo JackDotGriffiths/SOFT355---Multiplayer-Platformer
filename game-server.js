6bf21e788a4911e84044904f8231fe5008b9006d
JackGriffiths <jackdotgriffiths@gmail.com>
2020-01-05 12:51:40 +0000

    Socket.io implemented, and player names

--- game-server.js
+++ game-server.js
@@ -3,17 +3,41 @@ var express = require('express');
 var finalhandler = require('finalhandler');
 var serveStatic = require('serve-static');
 var microtime = require('microtime');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io').listen(server);
+
 
 var serve = serveStatic("./");
-var app = express();
 var currentLevel = 1;
 var nextLevel = 2;
 var previousTime;
 
+//Multiplayer
+var players = {};
+
 
 app.use(express.static('./'));//Serving static file
 
-app.listen(9000, function() { //Listener for specified port
+io.on('connection', function(socket){
+  var possibleNames = ["Koala","Giraffe","Hippo","Gorilla","Elephant","Frog"];
+  var randomInt = Math.floor(Math.random() * Math.floor(possibleNames.length));
+  players[socket.id] = {
+    playerId : socket.id,
+    playerName : possibleNames[randomInt]
+  };
+  console.log('+ PLAYER CONNECTED + ' + players[socket.id].playerName + ' has connected from ' + socket.id);
+  socket.emit('currentPlayers', players);
+  socket.broadcast.emit('newPlayer', players[socket.id]);
+
+  socket.on('disconnect', function(){
+  console.log('- PLAYER DISCONNECTED - A user has disconnected from ' + socket.id)
+  delete players[socket.id];
+  io.emit('disconnect',socket.id);
+  })
+})
+
+server.listen(9000, function() { //Listener for specified port
     previousTime = microtime.now();
     setInterval(createLevel,100);
     console.log("Server running at: http://localhost:" + 9000)
@@ -43,7 +67,7 @@ function createLevel(){ // Generates the id for the next level.
       console.log("-> SERVER DELAY <- Warning: Discrepancy of " + discrepancy + " ticks detected. ");
       console.log("-------------------------------------------------------------------------------");
     }
-    console.log("-GENERATING- Incoming section " + nextLevel + " | Delay of " + difference + " ticks | Discrepancy of " + (difference -5000000));
+    console.log("     -> GENERATING- Incoming section : " + nextLevel + " | Delay : " + difference + " ticks | Discrepancy : " + (difference -5000000));
     previousTime = microtime.now();
   }
 }
